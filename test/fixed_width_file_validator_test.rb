f452c47fcf6914d17dd688592b8829835297356c
Li Lin <guru.lin@gmail.com>
2018-03-18 17:09:21 +0800

    include other tests

--- test/fixed_width_file_validator_test.rb
+++ test/fixed_width_file_validator_test.rb
@@ -1,4 +1,8 @@
 require_relative 'test_helper'
+require_relative 'string_helper_test'
+require_relative 'field_validator_test'
+require_relative 'file_reader_test'
+require_relative 'file_format_config_test'
 
 class FixedWidthFileValidatorTest < Minitest::Test
   def test_that_it_has_a_version_number
