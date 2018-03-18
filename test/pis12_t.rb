bbcd80c008e354ad86eab6655d8d49a81ac16b62
Li Lin <guru.lin@gmail.com>
2018-03-18 20:02:14 +0800

    rename the file so that it will not be run as part of build

---
+++ test/pis12_t.rb
@@ -0,0 +1,28 @@
+require_relative 'test_helper'
+
+class RuleParserTest < Minitest::Test
+  def test_can_parse_tandem_sample_file
+    data_file_path = 'test/data/test_data_1.txt'
+    format = FixedWidthFileValidator::FileFormat.for(:tandem, 'test/data/pis12.yml')
+    reader = format.create_file_reader(data_file_path)
+    validator = format.create_record_validator_with_reader(data_file_path)
+
+    errors = validator.find_all_errors(reader)
+    other_errors = errors.reject { |r| r[:validation] == 'unique' }
+
+    assert_equal 4, other_errors.count
+  end
+
+  def test_can_parse_c400_sample_file
+    skip
+    puts "Start: #{Time.now}"
+    data_file_path = '/Users/lee/tmp/CRTRAN25AEC40012052017193043.txt'
+    format = FixedWidthFileValidator::FileFormat.for(:tandem, 'test/data/pis12.yml')
+    reader = format.create_file_reader(data_file_path)
+    validator = format.create_record_validator_with_reader(data_file_path)
+    errors = validator.find_all_errors(reader)
+    puts "End: #{Time.now}"
+
+    assert errors.count
+  end
+end
