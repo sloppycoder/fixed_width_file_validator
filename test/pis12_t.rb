af21de7037a6d3b2406fa8a3db56a0faa71f12c6
Li Lin <li.lin2@sc.com>
2018-03-19 13:53:54 +0800

    fixed data file path

--- test/pis12_t.rb
+++ test/pis12_t.rb
@@ -2,14 +2,13 @@ require_relative 'test_helper'
 
 class RuleParserTest < Minitest::Test
   def test_can_parse_tandem_sample_file
-    data_file_path = 'test/data/test_data_1.txt'
+    data_file_path = 'test/data/PIS12TWTDEM09042017140418.txt'
     format = FixedWidthFileValidator::FileFormat.for(:tandem, 'test/data/pis12.yml')
     reader = format.create_file_reader(data_file_path)
     validator = format.create_record_validator_with_reader(data_file_path)
 
     errors = validator.find_all_errors(reader)
-    other_errors = errors.reject { |r| r[:validation] == 'unique' }
-
+    other_errors = errors.reject { |r| r.failed_validation == 'unique' }
     assert_equal 4, other_errors.count
   end
 
